7c2cabde63345094cff0dc24f8329b3c5c2620a1
its-Pradyot <itkurkarpradyot@gmail.com>
2021-01-04 11:37:34 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,35 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
 
   mydate = new Date();
 
-  emp = {"empid":12,"empname":"ABCD","salary":26000}
+  emp = { empid: 12, empname: "ABCD", salary: 26000 };
 
-  cities = ['Pune','Mumbai','Nagpur','Goa','Nashik'];
+  cities = ["Pune", "Mumbai", "Nagpur", "Goa", "Nashik"];
 
+  mydata = "abcd";
+  myname = "";
 
+  empid = "";
+  empname = "";
+  salary = "";
+  location = "";
+
+  hello() {
+    this.mydata = this.myname;
+  }
+
+  hello1() {
+    this.empid = this.empid;
+    this.empname = this.empname;
+    this.salary = this.salary;
+    this.location = this.location;
+  }
 }
